637dbeb7efe6c5c68d14afe4ced8da02809d8c12
MH Manuel Haqiqatkhah <mhscientist@gmail.com>
2021-06-14 19:46:30 +0200

    2021-06-14 snp corrected, full (zipped) & lean & only-1e+6 saved

--- scripts/20210613_correcting-snp.R
+++ scripts/20210613_correcting-snp.R
@@ -1,20 +1,31 @@
 
 # Notes -------------------------------------------------------------------
-
-## snp data has two flaws:
-## 1. small-world index is calculated incorrectly (changing C*E to C/APL)
-## 2.family names must be corrected:
+#############################
+######## 2021-06-14 #########
+#############################
+## snp data objects prior to 2021 had two flaws:
+## 1.family names must be corrected:
 ##    - Under coupled UC --> sub-coupled SC
 ##    - Over coupled OC --> hyper-coupled HC
 ##    - over-turbulent OT --> hyper-divergent HD
 ##    - underturbulent UT --> sub-divergent SD
-
+## 2. small-world index is calculated incorrectly (changing C*E to C/APL)
+##
 ## So we first load the complete snp, make changes, and save versions of it
-# system.time(
-#   load("./data/snp_all_5k_20190712_1730.RData")
-# )
+## These will take time (~10 minutes)
+
+
+# Reading snp
+
+load("./functions/functions_save_vars.R")
+
+system.time(
+  load("./data/snp_all_5k_20190712_1730.RData")
+)
 
 snp.tmp <- snp
+
+# Correcting family labels and names
 
 snp.tmp$Verbal.Description <-
   plyr::mapvalues(snp.tmp$Verbal.Description,
@@ -29,51 +40,39 @@ snp.tmp$Verbal.Description <-
                          "SD_Sub-divergent minority",
                          "SC_Sub-coupled minority")
                  )
-                  
-(family.code <- snp.tmp$Verbal.Description %>%
-  as.character() %>% 
-  sort(decreasing = FALSE) %>% 
-  substr(1,2) %>% 
-  rep(each=10) %>% 
-  paste0(rep(rep(c(1:50), each=800)),
-         "_",
-         .)
-  )
 
-(uu <- family.code %>% unique())
-
-rep()
-
-snp.tmp2 <- snp.tmp %>% 
-  add_column(fn = paste0(snp.tmp$Verbal.Description,
+snp.tmp <- snp.tmp %>% 
+  add_column(fn = paste0(rep(c(1:50),
+                             each=800),
+                         "_",
+                         snp.tmp$Verbal.Description,
                          "_",
                          snp.tmp$Owner),
              .after = 6) %>%
   arrange(fn) %>%
-  # select(-adj.mat.vect) %>% 
-  add_column(fn = paste0(rep(c(3,2,1,4,5),each=10),
-                         "_",
-                         snp.tmp$fn),
-             .after = 7)
-  arrange(fn) %>%
-  cbind(family.codes = families, num =1:50) %>%
-  mutate(newcodes = paste0(num,"_",family.codes,rep(c(1:10), 5), "_", Owner)) %>%
-  mutate(famcode.num = paste0(family.codes, rep(c(1:10), 5)))
-
-
+  add_column(ID = paste0(snp.tmp$Verbal.Description %>%
+                           as.character() %>% 
+                           sort(decreasing = FALSE) %>%
+                           substr(1,2),
+                         rep(
+                           rep(c(1:10),
+                               each=800),
+                           5)
+                         ),
+             .after = 1)
   
-xx <- 1
-ddd[(800*xx-8):(800*xx+8),]
-  
+# Correcting small-world index (changing C*E to C/APL)
+
+snp.tmp$Small.World <- snp.tmp$Clustering/snp.tmp$Average.Path.Length
 
 
-# families <<- c("OC", "OT", "BL", "UT", "UC") %>% rep(each = 10)
-families <<- c("HC", "HD", "BL", "SD", "SC") %>% rep(each = 10)
+# saving snp and variants back
 
-### correcting collumn names in snp
+snp <- snp.tmp
+save_vars("snp", prefix = "snp_all_5k")
 
-# correcting small-world index (changing C*E to C/APL)
+snp.lean <- snp.tmp %>% select(-adj.mat.vect)
+save_vars("snp.lean", prefix = "snp.lean_all_5k")
 
-snp.lite <- snp[sample(50,10),1:16]
-
-snp$Small.World <- snp$Clustering/snp$Average.Path.Length
+snp <- snp.tmp %>% filter(Rewiring == 1000000)
+save_vars("snp", prefix = "snp_only-1e+6")
